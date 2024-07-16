14402681bcc82aeae8521a78dacf4163d346a256
Arjun Jakkipally <68869672+arjunj132@users.noreply.github.com>
2024-07-16 14:50:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from ai import generate
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return generate()
+
+@app.route('/about')
+def about():
+    return 'GitaQuestionAI is a Llama3 and Mixtral-8x7B-Instruct-v0.1 based AI program designed to make 10 shlokas question (can be very unreliable). By GitaGuru'
